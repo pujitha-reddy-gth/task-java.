b4c504ae78c5b998d67ee038f208e1a1f0cef4bf
pujitha-reddy-gth <pujithathammaneni@gmail.com>
2025-05-29 22:46:29 +0530

    Add files via upload

---
+++ Main4.java
@@ -0,0 +1,11 @@
+package com.inheritance;
+
+public class Main4 {
+
+
+	    public static void main(String[] args) {
+	        EBook defaultEBook = new EBook();
+	        EBook titledEBook = new EBook("Java Programming");
+	    }
+	}
+
