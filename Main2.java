c9d87739664bdefb958a85fc2b78147005a85f2c
pujitha-reddy-gth <pujithathammaneni@gmail.com>
2025-05-29 22:44:09 +0530

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,10 @@
+package com.inheritance;
+
+public class Main2 {
+
+
+	    public static void main(String[] args) {
+	        Student student = new Student(" John Doe", "Alice Smith");
+	        student.printNames();
+	    }
+	}
